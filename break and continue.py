58210ca7736da9947aff8dfef0c9aeea2ac88c87
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-24 23:23:06 +0530

    Add files via upload

---
+++ break and continue.py
@@ -0,0 +1,34 @@
+#while(1) and while(true) are loops which run endlessly
+'''i=0
+while(1):
+    print(i)
+    i=i+1 '''
+
+    #using break statement
+'''i=0
+while(True):
+    print(i,end='')
+    if(i==20):
+        break
+    i=i+1'''
+
+'''i=1
+while(1):
+    print(i)  
+    if i==105:
+        break
+    i=i+2'''
+
+i=0
+while(1):  
+    if(i+1<5) :
+        continue
+    print(i+1) 
+    if(i==45):
+        break
+    i=i+1
+    
+
+
+    
+
