56e436d82f32c06834121b126f7579068adda187
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-24 23:20:55 +0530

    Add files via upload

---
+++ maximum of 3 nos.py
@@ -0,0 +1,20 @@
+def max_no(a,b,c):
+    if a>b and a>c:
+        print("a is the greatest")
+        
+    elif b>a and b>c:
+        print("b is the greatest")
+        
+    else:
+        print("c is greater than thr rest ")
+
+max_no(1,2,3)  
+
+def greatest(a,b,c):
+  if a>b and a>c:
+    print("a is great")
+  elif b>a and b>c:
+    print("b is great")   
+  else:
+    print("c is great")  
+greatest(89,2,3)
