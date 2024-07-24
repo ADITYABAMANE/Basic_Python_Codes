f2e15b8a93656ad5058187a3115443dd936a62ff
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-24 23:21:45 +0530

    Add files via upload

---
+++ sum .py
@@ -0,0 +1,18 @@
+def sum(a,b):
+    w=a+b
+    print(w)
+sum(2,3) 
+
+
+def add(j,k):
+    v=j+k
+    return(v)
+result=add(3,2)
+print(result)
+
+
+def add(s,d):
+    k=s+d
+    return(k)
+ans=add(6,7)   
+print(ans)
