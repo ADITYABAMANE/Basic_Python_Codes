d9f963d10e049f3b51ae6913bed1534377b2755c
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-25 21:14:12 +0530

    Add files via upload

---
+++ variables and datatypes and typecasting.py
@@ -0,0 +1,34 @@
+var1="aditya"
+var2=12
+var3=13.34
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+
+var1="67"
+var2="89"
+print(var1+var2)
+
+var1="45"
+var2="50"
+print(int(var1)+int(var2))
+
+
+
+
+
+
+
+var1="10"
+var2="10"
+print(100*str(int(var1)+int(var2))) 
+
+
+print("enter a value=")
+inpnum=input()
+print("no got  is",int(inpnum)*2)
+
+
+
+
