f1701a5afc337c91892ace5c927a71c10671a3a0
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-25 21:14:34 +0530

    Add files via upload

---
+++ while Loop.py
@@ -0,0 +1,35 @@
+i=2
+while(i<45):
+    print(i)
+    i=i+2
+
+
+
+num=1
+print("numbers\t squares")   
+while(num<=10):
+    print(num,"\t" , num**2)
+    num=num+1
+
+num=10
+while(num<=300):
+    print(num,end="-")
+    num=num+10
+
+num=105
+while(num<=7):
+    print(num)
+    num=num-7
+
+num=1
+while(num<=10):
+    print(num,end=",")
+    num=num+1
+
+
+num=1
+while(num<=10):
+    print("multiplication table",num*6)
+    num=num+1
+
+
