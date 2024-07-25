227694ce44ac713626da8d8bb215882e7805bce4
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-25 21:13:34 +0530

    Add files via upload

---
+++ short hand if else.py
@@ -0,0 +1,10 @@
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+
+
+print("a is greater than b") if(a>b) else print("b is greateer than a")
+
+
+w=int(input("enter your age="))
+print("eligible to vote ") if w>18 else print("not eligible to vote")
