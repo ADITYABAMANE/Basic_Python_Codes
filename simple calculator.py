4202ce9b7ecb1aec25822088da31a5d28edf1717
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-07-25 21:13:59 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,18 @@
+#making a simple calculator which only does the operation of adding two numbers
+
+print("enter a n1=")
+n1=input()
+print("enter a number=")
+n2=input()
+
+print("enter a no=",int(n1)+int(n2))
+
+
+
+
+
+
+
+ 
+
+
