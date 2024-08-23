4bcabb994826604b9df5f854b164669803162e6e
Aditya Bamane <147440251+ADITYABAMANE@users.noreply.github.com>
2024-08-23 23:16:20 +0530

    Add files via upload

---
+++ python/aditya.py
@@ -0,0 +1,31 @@
+'''l1=[]
+print("l1")
+l1.append(12)
+print("l1=",l1)'''
+
+'''a= int (input("marks in maths="))
+b=int(input("marks in science"))
+percentage=a+b/200
+print("percentage=",percentage)
+
+if(percentage>35):
+    print("student is pass")
+if(percentage<35):
+    print("fail")'''
+
+items=[("aditya"),32,67,3]
+for item in items:
+     if str(item).isnumeric() and item>15:
+        print(item)
+
+
+
+
+
+
+
+
+
+
+
+
